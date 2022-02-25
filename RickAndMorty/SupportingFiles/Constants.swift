52a169e64f14e0c2e75950944d8f5e9a5e7e7fb0
MityaAndreevich <dmitry.logachev@icloud.com>
2022-02-25 19:23:44 +0300

    add model for RickAndMorty api

---
+++ RickAndMorty/SupportingFiles/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RickAndMorty
+//
+//  Created by Dmitry Logachev on 25.02.2022.
+//
+
+//import Foundation
+
+struct K {
+    static let apiURL = "https://rickandmortyapi.com/api/character"
+}
