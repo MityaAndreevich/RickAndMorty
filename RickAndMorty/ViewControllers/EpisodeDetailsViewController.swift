cbc9278cfea96e260da3f5ad50fcb5116cee4bcf
MityaAndreevich <dmitry.logachev@icloud.com>
2022-03-22 17:55:52 +0300

    add episodesDetailsVC content

--- RickAndMorty/ViewControllers/EpisodeDetailsViewController.swift
+++ RickAndMorty/ViewControllers/EpisodeDetailsViewController.swift
@@ -9,24 +9,66 @@ import UIKit
 
 class EpisodeDetailsViewController: UIViewController {
     
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var episodeSescriptionLabel: UILabel!
+    
     var episode: Episode!
-    private var characters: [Character] = []
+    private var characters: [Character] = [] {
+        didSet {
+            if characters.count == episode.characters.count {
+                characters = characters.sorted { $0.id < $1.id }
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setCharacters()
+        tableView.backgroundColor = UIColor(
+            red: 21/255,
+            green: 32/255,
+            blue: 66/255,
+            alpha: 1
+        )
+        title = episode.episode
+        episodeSescriptionLabel.text = episode.description
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let detailsVC = segue.destination as! CharacterDetailsViewController
+        detailsVC.character = sender as? Character
     }
-    */
+    
+    private func setCharacters() {
+        episode.characters.forEach { characterURL in
+            NetworkManager.shared.fetchCharacter(from: characterURL) { character in
+                self.characters.append(character)
+            }
+        }
+    }
+}
 
+//MARK: - TableView Data Source
+extension EpisodeDetailsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        episode.characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        let charactersURL = episode.characters[indexPath.row]
+        NetworkManager.shared.fetchCharacter(from: charactersURL) { character in
+            cell.configure(with: character)
+        }
+        return cell
+    }
+}
+
+//MARK: - TableView Delegate
+extension EpisodeDetailsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let character = characters[indexPath.row]
+        performSegue(withIdentifier: "ShowCharacter", sender: character)
+    }
 }
