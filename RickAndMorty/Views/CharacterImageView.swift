f3dbe6aa21a1ef5e00e49b371b9c515ce4cb1bfd
MityaAndreevich <dmitry.logachev@icloud.com>
2022-03-26 20:28:02 +0300

    add cached data possibility add launch screen

--- RickAndMorty/Views/CharacterImageView.swift
+++ RickAndMorty/Views/CharacterImageView.swift
@@ -13,10 +13,32 @@ class CharacterImageView: UIImageView {
             image = UIImage(named: "picture")
             return
         }
+        // if we have image in cache we get it from there
+        if let cachedImage = getCachedImage(from: url) {
+            image = cachedImage
+            return
+        }
         
+        // If no data in cache we download it from url
         ImageManager.shared.fetchImage(from: url) { data, response in
             self.image = UIImage(data: data)
+            self.saveDataToCache(with: data, and: response)
         }
+    }
+    
+    private func saveDataToCache(with data: Data, and response: URLResponse) {
+        guard let url = response.url else { return }
+        let request = URLRequest(url: url)
+        let cashedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cashedResponse, for: request)
+    }
+    
+    private func getCachedImage(from url: URL) -> UIImage? {
+        let request = URLRequest(url: url)
+        if let cachedResponse = URLCache.shared.cachedResponse(for: request) {
+            return UIImage(data: cachedResponse.data)
+        }
+        return nil
     }
 
 }
